0f9984c34ed427546f78d6d39c6bf75d85ddd2d2
peterlanier <peterlanier@me.com>
2020-04-28 16:53:41 -0400

    style footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,18 @@
 import React from "react"
 
 const Footer = () => (
-  <footer
-    style={{}}
-  >
-  <div
+  <footer style={{}}>
+    <div
       style={{
-      maxWidth: 720,
-      margin: `0 auto`,
-      padding: `1rem 1.0875rem`,
-    }}
+        maxWidth: 720,
+        margin: `0 auto`,
+        padding: `1rem 1.0875rem`,
+        display: `flex`,
+        justifyContent: `center`,
+        borderTop: `1px solid #eaecef`
+      }}
     >
-    Powered by Gatsby, React, GraphQL, Markdown |
-    © {new Date().getFullYear()}
+      Peter Lanier | © {new Date().getFullYear()}
     </div>
   </footer>
 )
