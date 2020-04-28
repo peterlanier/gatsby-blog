1a7010692ff1c2271b96b23d63d4afffcff11307
peterlanier <peterlanier@me.com>
2020-04-28 16:43:40 -0400

    rebuild header with grid and flex, social icons

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Peter from "../images/peter.jpeg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 function Header({ siteTitle }) {
   return (
@@ -13,32 +15,65 @@ function Header({ siteTitle }) {
     >
       <div
         style={{
-          margin: `0 auto`,
+          padding: `0 1.0875rem`,
           maxWidth: 720,
-          padding: `1.45rem 1.0875rem`,
+          margin: `0 auto`,
+          paddingTop: `1.45rem`,
         }}
       >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
+        <div
+          style={{
+            display: `grid`,
+            gridTemplateColumns: `auto auto`,
+          }}
+        >
+        <Link
+          to="/"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+            display: `grid`,
+            gridTemplateColumns: `60px auto`,
+          }}
+        >
+          <img
+            src={Peter}
+            alt="avatar"
             style={{
-              color: `black`,
-              textDecoration: `none`,
+              maxWidth: "60px",
+              borderRadius: "50%",
+              marginBottom: 0,
+            }}
+          />
+          <h1
+            style={{
+              margin: 0,
+              marginLeft: `0.5rem`,
+              display: `flex`,
+              justifyContent: `flex-start`,
+              alignItems: `center`,
+              maxWidth: `inherit`,
             }}
           >
-            <img
-              src={Peter}
-              alt="avatar"
-              style={{
-                maxWidth: "60px",
-                borderRadius: "50%",
-                marginBottom: "-20px",
-                marginRight: "15px",
-              }}
-            />
             {siteTitle}
-          </Link>
-        </h1>
+          </h1>
+        </Link>
+        <div
+          className="header-social"
+          style={{
+            display: `flex`,
+            justifyContent: `flex-end`,
+            alignItems: `center`,
+          }}
+        >
+          <a href="https://www.linkedin.com/in/peter-lanier/" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} size="lg" color="black" />
+          </a>
+          <a href="https://github.com/peterlanier/" target="_blank" style={{marginLeft: `0.5rem`}}>
+            <FontAwesomeIcon icon={faGithubSquare} size="lg" color="black" />
+          </a>
+        </div>
+      </div>
       </div>
     </header>
   )
--- src/components/hero.js
+++ src/components/hero.js
@@ -2,48 +2,107 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Peter from "../images/peter.jpeg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 function Hero({ siteTitle }) {
   return (
     <header
       style={{
         background: `#23282E`,
-        marginBottom: `1.45rem`,
+        marginBottom: `2rem`,
       }}
     >
       <div
         style={{
-          margin: `0 auto`,
+          padding: `0 1.0875rem`,
           maxWidth: 720,
-          padding: `1.45rem 1.0875rem`,
+          margin: `0 auto`,
+          paddingTop: `1.45rem`,
         }}
       >
-        <h1 style={{ margin: 0 }}>
+        <div
+          style={{
+            display: `grid`,
+            gridTemplateColumns: `auto auto`,
+          }}
+        >
           <Link
             to="/"
             style={{
-              color: `white`,
+              color: `#F5F5F5`,
+              
               textDecoration: `none`,
+              display: `grid`,
+              gridTemplateColumns: `60px auto`,
             }}
           >
             <img
               src={Peter}
               alt="avatar"
               style={{
-                maxWidth: "64px",
+                maxWidth: "60px",
                 borderRadius: "50%",
-                marginBottom: "-20px",
-                marginRight: "15px",
-                border: '2px solid white'
+                marginBottom: 0,
               }}
             />
-            {siteTitle}
+            <h1
+              style={{
+                margin: 0,
+                marginLeft: `0.5rem`,
+                display: `flex`,
+                justifyContent: `flex-start`,
+                alignItems: `center`,
+                maxWidth: `inherit`,
+              }}
+            >
+              {siteTitle}
+            </h1>
           </Link>
-        </h1>
-        <p style={{ color: "white", marginTop: "1.45rem", maxWidth: "720px" }}>
-          I'm a software engineer, a web developer, and a lifelong learner. I'm currently passionate about React & serverless integrations.
-          This is my blog.
-        </p>
+          <div
+            className="header-social"
+            style={{
+              display: `flex`,
+              justifyContent: `flex-end`,
+              alignItems: `center`,
+            }}
+          >
+            <a href="https://www.linkedin.com/in/peter-lanier/" target="_blank">
+              <FontAwesomeIcon icon={faLinkedin} size="lg" color="#F5F5F5" />
+            </a>
+            <a
+              href="https://github.com/peterlanier/"
+              target="_blank"
+              style={{ marginLeft: `0.5rem` }}
+            >
+              <FontAwesomeIcon
+                icon={faGithubSquare}
+                size="lg"
+                color="#F5F5F5"
+              />
+            </a>
+          </div>
+        </div>
+
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 720,
+            paddingBottom: `0.1rem`,
+          }}
+        >
+          <p
+            style={{
+              color: "#F5F5F5",
+              marginTop: "1.45rem",
+              maxWidth: "680px",
+            }}
+          >
+            I'm a software engineer, a web developer, and a lifelong learner.
+            I'm currently passionate about React & serverless integrations. This
+            is my blog.
+          </p>
+        </div>
       </div>
     </header>
   )
